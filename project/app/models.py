52f09e4e73ba295736accf59dc3c9978c2c7c845
Michał Kalicki <kalicki@michas-air.home>
2025-01-10 11:30:45 +0100

    Add Category, Institution and Donation models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(choices=(
+        ('0', 'Fundacja'),
+        ('1', 'Organizacja Pozarządowa'),
+        ('2', 'Zbiórka Lokalna'),
+    ), default='0', max_length=25)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, default=None)
