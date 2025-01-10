bc427fc42f76d32ef49d347440f29aa883ee09be
Michał Kalicki <kalicki@michas-air.home>
2025-01-10 11:06:08 +0100

    Refactor html files, add base.html file, add first views

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,22 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'app/index.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'app/form.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'app/login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'app/register.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPageView.as_view(), name='landing-page'),
+    path('donation/', views.AddDonationView.as_view(), name='add-donation'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
