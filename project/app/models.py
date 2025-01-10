2b4e3c1423a73f7b9ebb884d06214f73e4d20149
Michał Kalicki <kalicki@michas-air.home>
2025-01-10 15:29:32 +0100

    Change username field with email in django User model, db reset

--- project/app/models.py
+++ project/app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
 
 
 class Category(models.Model):
