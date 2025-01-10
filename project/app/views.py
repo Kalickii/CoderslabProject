daa6ad5f8ec54003ea006c806335529335bc6077
Michał Kalicki <kalicki@michas-air.home>
2025-01-10 12:31:53 +0100

    Main page -> bags + supported institutions statistics

--- project/app/views.py
+++ project/app/views.py
@@ -1,10 +1,19 @@
 from django.views import View
 from django.shortcuts import render
 
+from app.models import Donation, Institution
+
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'app/index.html')
+        bag_count = sum(donation.quantity for donation in Donation.objects.all())
+        supported_institutions = sum(1 for institution in Institution.objects.all() if institution.donation_set.all().count() > 0)
+
+        ctx = {
+            'bag_count': bag_count,
+            'supported_institutions': supported_institutions,
+        }
+        return render(request, 'app/index.html', ctx)
 
 
 class AddDonationView(View):
